27f4e167ca3bccf50d355330ba2898dae19aa4ed
Bruno Moura <bruno8moura@gmail.com>
2020-06-19 05:49:27 -0300

    Get all repositories once when page is loaded.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    try {
+      loadRepos();
+    } catch (error) {
+      console.log('Error: ', error);
+    }
+
+  }, []);
+
+  async function loadRepos() {
+    const resp = await api.get('repositories');
+    setRepositories([...resp.data]);
+  }
+
   async function handleAddRepository() {
     // TODO
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+      // TODO
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {
+          repositories.map(repository => (
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          )
+          )
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
